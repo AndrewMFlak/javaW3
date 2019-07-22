917ef4fc42a92ed4a499392471378748c4c56cc9
AFlak <AndrewMFlak@gmail.com>
2019-07-22 15:05:56 -0400

    updated push

---
+++ Concatenator.java
@@ -0,0 +1,9 @@
+public class Concatenator{
+    public static void main(String[] args) {
+        String firstName = "Joe ";
+        String lastName = "Mama";
+        String concatenatedStuff = firstName + lastName;
+
+        System.out.println(concatenatedStuff);
+    }
+}
