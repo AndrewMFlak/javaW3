5ede104b995acaf4a063d8de82a02cdd6d4a33e5
AFlak <AndrewMFlak@gmail.com>
2019-07-25 22:50:35 -0400

    updated push of content

---
+++ Primitive.java
@@ -0,0 +1,9 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int myInt = 234;
+        double myDouble = 987321.123456;
+        double myEffinDouble = myInt + myDouble;
+        System.out.println(myEffinDouble);
+
+    }
+}
