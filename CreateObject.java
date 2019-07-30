b52cc60c4d3efae49581723b0c4ab2e4db7f3626
AFlak <AndrewMFlak@gmail.com>
2019-07-29 22:24:07 -0400

    amended object

--- CreateObject.java
+++ CreateObject.java
@@ -7,8 +7,7 @@ public class CreateObject {
         System.out.println("Your java canned function has finished running.  Keep at it.  Rome was not built in a day.");
     }
 
-    public void doSomeShit() {
-        long number = 0;
+    public void doSomeShit(long number) {
         while (number < 1000001) {
             System.out.println(number);
             number = number + 10000;
@@ -18,8 +17,9 @@ public class CreateObject {
 
     public static void main(String[] args) {
         printFunction();
+        long number = 0;
         CreateObject someObject = new CreateObject();
-        someObject.doSomeShit();
+        someObject.doSomeShit(number);
         printConclusion();
     }
 }
