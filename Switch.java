3cc2cc4d97bb84f8ca1a36810e64fe4cf02cf7ea
AFlak <AndrewMFlak@gmail.com>
2019-08-01 22:40:30 -0400

    updated

--- Switch.java
+++ Switch.java
@@ -1,13 +1,30 @@
 public class Switch {
-    static void closer(){
-        System.out.println("Fuck yes this is a java app!");
-    }
+    // static void closer(){
+    //     System.out.println("Fuck yes this is a java app!");
+    // }
+    // public static void main(String[] args) {
+    //     int aNumber = 0;
+    //     while(aNumber < 101) {
+    //         System.out.println(aNumber);
+    //         aNumber++;
+    //     };
+    //     closer();
+    // }
     public static void main(String[] args) {
-        int aNumber = 0;
-        while(aNumber < 101) {
-            System.out.println(aNumber);
-            aNumber++;
-        };
-        closer();
+        int someNumber = 10;
+        
+        switch(someNumber) {
+            case 0:
+                System.out.println("Your number is " + 0);
+                break;
+            case 5:
+                System.out.println("Your number is " + 5);
+                break;
+            case 10: 
+                System.out.println("Your number is " + 10);
+                break;
+            default:
+                System.out.println("Default value selected biatch!");
+        }
     }
 }
