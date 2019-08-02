e0e73c571941dc702df37602f163a04ea99d5409
AFlak <AndrewMFlak@gmail.com>
2019-08-01 22:28:51 -0400

    updated push of stuff

---
+++ MathClass.java
@@ -0,0 +1,17 @@
+public class MathClass {
+    public static void main(String[] args) {
+        int x = 5;
+        x += 5;
+        System.out.println(x);
+
+
+        int y = 10;
+        y <<= 4;
+        System.out.println(y);
+
+
+        double z = Math.random();
+        System.out.println(z);
+
+    }
+}
---
+++ Switch.java
@@ -0,0 +1,13 @@
+public class Switch {
+    static void closer(){
+        System.out.println("Fuck yes this is a java app!");
+    }
+    public static void main(String[] args) {
+        int aNumber = 0;
+        while(aNumber < 101) {
+            System.out.println(aNumber);
+            aNumber++;
+        };
+        closer();
+    }
+}
