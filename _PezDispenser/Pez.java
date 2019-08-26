7bd0cce29db2da31cabab1ec5c266862a49c9d5a
AFlak <AndrewMFlak@gmail.com>
2019-08-26 14:47:15 -0400

    updated push of content

---
+++ _PezDispenser/Pez.java
@@ -0,0 +1,14 @@
+class Pez {
+
+    // To enter JShell in the command prompt use the command 'jshell';
+    // To exit JShell from the command propmt use the command prompt '/exit'
+    public static final int MAX_BARS = 12;
+    final private String characterName;
+    private int pezCount;
+    public Pez(String characterName) {
+        this.characterName = characterName;
+        pezCount = 0;
+
+    }
+
+}
